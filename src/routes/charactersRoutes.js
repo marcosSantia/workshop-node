56b3e5b842ac188a1ccc9ab35f6a5d6b4cec38aa
Marcos Santia <maycolsantia@gmail.com>
2024-03-14 19:57:26 -0300

    [FEAT] characters controllers

--- src/routes/charactersRoutes.js
+++ src/routes/charactersRoutes.js
@@ -1,40 +1,16 @@
 const express = require('express');
 const router = express.Router();
-
-router.get('/characters', (req, res)=>res.send('En teoria esto deberia devolver todos los caracteres pero aun no se quien me los va dar.'));
-
+const charactersControllers = require('../controllers/charactersControllers');
 
 
+router.get('/characters', charactersControllers.getCharacters);
 
-         /*path params porque son parametros de la ruta*/
-router.get('/character/:id?', (req,res) => {
-    
-    /* aca hago los queryparams que son los que vienen despues del signo de pregunta */
-    const id= req.params.id;
-    const queryParams = req.query;
-    console.log(queryParams); 
-    /* 
-    Entonces, en postman me va seguir devolviendo el res.send con el mensaje, pero en la consola me devuelve los queryparams que hardcode por get en postman localhost:4001/character/1?order=asc&filter=new 
+/*path params porque son parametros de la ruta*/
+router.get('/character/:id?', charactersControllers.getCharacter);
 
-    otra forma: Como se que le estoy mandand order y filter podria hacer
-    
-    const {order, filter} = req,params;
-    console.log(order);
-    console.log(filter);
+router.post('/character/create', charactersControllers.createCharacter);
 
-    basicamente le aplico destructiring al objeto. y los tomo por separado
-    
-    En resumen los pathParams son los que a mi me van a ayudar a identificar cuestiones especificas de la ruta y los queryParams son los parametros que me van ayudar a identificar caracteristicas diferentes de una misma ruta, porque a una ruta la podes recibir con order:asc o order:desc o con filter:xxx etc etc. Su uso podria ser para filtrar por precio por nuevo por orden etc, buscar en tablas de busquedas */
-    res.send('En teoria yo tendria que devolver el character con el id proporcionado pero aun no se quien me lo va dar.')
-});
-
-
-
-
-
-router.post('/character/create', (req,res)=>res.send('En teoria yo tendria que enviar los datos para crear un nuevo character'));
-
-router.delete('/character/:id/delete', (req, res)=>res.send('En teoria tendria que enviar los datos para eliminar un character en funcion del ID proporcionado.'));
+router.delete('/character/:id/delete', charactersControllers.deleteCharacter);
 
 
 module.exports = router;
